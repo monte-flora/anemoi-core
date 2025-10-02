42c0194662cfbcc6d5ae09a2a590848d7169753f
Mariana Clare <31656450+mc4117@users.noreply.github.com>
2025-10-02 11:09:27 +0200

    fix: interactive mode (#578)
    ## Description
    @fprill reported that following #334 transfer learning no longer works
    in interactive mode
    
    ## What problem does this change solve?
    This issue is now fixed by stopping the model from using a feature from
    the DDP strategy in ineractive mode
    
    
    ***As a contributor to the Anemoi framework, please ensure that your
    changes include unit tests, updates to any affected dependencies and
    documentation, and have been tested in a parallel setting (i.e., with
    multiple GPUs). As a reviewer, you are also responsible for verifying
    these aspects and requesting changes if they are not adequately
    addressed. For guidelines about those please refer to
    https://anemoi.readthedocs.io/en/latest/***
    
    By opening this pull request, I affirm that all authors agree to the
    [Contributor License
    Agreement.](https://github.com/ecmwf/codex/blob/main/Legal/contributor_license_agreement.md)
    
    ---------
    
    Co-authored-by: fprill <4728053+fprill@users.noreply.github.com>

--- training/src/anemoi/training/diagnostics/callbacks/sanity.py
+++ training/src/anemoi/training/diagnostics/callbacks/sanity.py
@@ -32,6 +32,10 @@ class CheckVariableOrder(pl.callbacks.Callback):
         """
         data_name_to_index = trainer.datamodule.ds_train.name_to_index
 
+        if not hasattr(trainer.model, "module"):
+            LOGGER.warning("Dataset order is not being checked")
+            return
+
         if hasattr(trainer.model.module, "_ckpt_model_name_to_index"):
             self._model_name_to_index = trainer.model.module._ckpt_model_name_to_index
         else:
@@ -51,6 +55,10 @@ class CheckVariableOrder(pl.callbacks.Callback):
         """
         data_name_to_index = trainer.datamodule.ds_valid.name_to_index
 
+        if not hasattr(trainer.model, "module"):
+            LOGGER.warning("Dataset order is not being checked")
+            return
+
         if hasattr(trainer.model.module, "_ckpt_model_name_to_index"):
             self._model_name_to_index = trainer.model.module._ckpt_model_name_to_index
         else:
@@ -69,6 +77,10 @@ class CheckVariableOrder(pl.callbacks.Callback):
             Not used
         """
         data_name_to_index = trainer.datamodule.ds_test.name_to_index
+
+        if not hasattr(trainer.model, "module"):
+            LOGGER.warning("Dataset order is not being checked")
+            return
 
         if hasattr(trainer.model.module, "_ckpt_model_name_to_index"):
             self._model_name_to_index = trainer.model.module._ckpt_model_name_to_index
